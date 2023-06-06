0b700ff92785c11d5a9b66630f02454c79282eb9
Amir Sabahi <amir.sabahi@gmail.com>
2023-06-06 15:36:06 +0330

    Update mariadb-db-backup.sh
    - Get the remote location
    - Get username, pass and other info from command
    - Show help

--- mariadb-db-backup.sh
+++ mariadb-db-backup.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-# Do not use in production.
+# Get command-line arguments
+DB_USER="$1"
+DB_PASSWORD="$2"
+DB_NAME="$3"
+BACKUP_LOCATION="$4"
+TRANSFER_LOCATION="$5"
 
-# Prompt user for database credentials and name
-read -p "Enter database username: " USER
-read -s -p "Enter database password: " PASSWORD
-echo
-read -p "Enter database name: " DB_NAME
+# Function to display script usage
+function display_usage() {
+    echo "Usage: $0 <db_user> <db_password> <db_name> <backup_location> [-t <transfer_location>]"
+}
+
+# Check if any required argument is missing
+if [[ -z $DB_USER || -z $DB_PASSWORD || -z $DB_NAME || -z $BACKUP_LOCATION ]]; then
+    display_usage
+    exit 1
+fi
+
+# Check if the "-h" option is passed to display usage
+if [[ "$DB_USER" == "-h" || "$DB_PASSWORD" == "-h" || "$DB_NAME" == "-h" || "$BACKUP_LOCATION" == "-h" ]]; then
+    display_usage
+    exit 0
+fi
 
 # Set the backup path and filename
 BACKUP_DIR="/path/to/backup/directory"
@@ -14,12 +30,26 @@ BACKUP_FILE="$BACKUP_DIR/$(date +%Y-%m-%d_%H-%M-%S).tar.gz"
 
 # Generate the backup and compress it
 echo "Starting backup..."
-mariabackup --user=$USER --password=$PASSWORD --databases $DB_NAME --target-dir=$BACKUP_DIR --backup
+mariabackup --user=$DB_USER --password=$DB_PASSWORD --databases $DB_NAME --target-dir=$BACKUP_DIR --backup
 tar czf $BACKUP_FILE $BACKUP_DIR/$(basename $BACKUP_DIR)
 rm -rf $BACKUP_DIR/$(basename $BACKUP_DIR)
 
+# Check if the backup location exists, if not create it
+if [ ! -d "$BACKUP_LOCATION" ]; then
+    mkdir -p "$BACKUP_LOCATION"
+fi
+
+# Move the backup file to the specified location
+mv $BACKUP_FILE $BACKUP_LOCATION
+
+# Transfer the backup file to another location if specified
+if [[ -n $TRANSFER_LOCATION ]]; then
+    echo "Transferring backup file to $TRANSFER_LOCATION..."
+    cp "$BACKUP_LOCATION/$(basename $BACKUP_FILE)" "$TRANSFER_LOCATION"
+fi
+
 # Show progress while compressing
 echo "Compressing backup..."
-pv $BACKUP_FILE > $BACKUP_FILE.gz
+pv "$BACKUP_LOCATION/$(basename $BACKUP_FILE)" > "$BACKUP_LOCATION/$(basename $BACKUP_FILE).gz"
 
 echo "Backup completed"
